7133fcbb61551d2272f8912d15aaba0313491d96
Nicholas D'Amico <nickalan82@icloud.com>
2017-10-31 19:20:21 -0400

    All spec for models passing, working through spec for features/sign_up - sign_in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+  	self.nausea > self.happiness ? "sad" : "happy"
+  end
+
+  def current_user
+  	binding.pry
+  	@current_user ||= User.find_by(id: session[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
+# Static Controller Routes
+get '/home' => 'static#home'
 
+# Users Controller Routes
+root 'users#new'
+get '/users/new' => 'users#new'
+get '/users/:id', to: "users#show", :as => "user_path"
+post '/users' => 'users#create'
 end
